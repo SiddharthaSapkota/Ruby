64330776ca3a0e5bd155c921d94d828c51a6c609
SiddharthaSapkota <83570524+SiddharthaSapkota@users.noreply.github.com>
2024-04-28 10:33:10 +0545

    Add files via upload

---
+++ lesson1.rb
@@ -0,0 +1,11 @@
+name = "Odin"
+
+puts "Hello, #{name}" #=> "Hello, Odin"
+puts 'Hello, #{name}' #=> "Hello, #{name}"
+
+5.times {
+  |time| puts time
+  }
+
+  str = "Hello Ruby!"
+putc str            #prints one character at a timr (first character)
